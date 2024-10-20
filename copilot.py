08cd56fbbd0daa4d27b286d1528390a426432588
Carl Yu <carlyu99@live.cn>
2024-10-20 13:23:34 -0700

    Format with minor changes

--- copilot.py
+++ copilot.py
@@ -1,42 +1,36 @@
-
-from openai import OpenAI
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from tenacity import retry, wait_random_exponential, stop_after_attempt
 import os
+
+from llama_index.core import Settings, SimpleDirectoryReader, VectorStoreIndex
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from openai import OpenAI
+from tenacity import retry, stop_after_attempt, wait_random_exponential
+
+
 @retry(wait=wait_random_exponential(multiplier=1, max=40), stop=stop_after_attempt(5))
-def chat_completion_request(client, messages, model="gpt-4o",
-                            **kwargs):
+def chat_completion_request(client: OpenAI, messages, model="gpt-4o", **kwargs):
     try:
-        response = client.chat.completions.create(
-            model=model,
-            messages=messages,
-            **kwargs
-        )
+        response = client.chat.completions.create(model=model, messages=messages, **kwargs)
         return response
     except Exception as e:
         print("Unable to generate ChatCompletion response")
         print(f"Exception: {e}")
         return e
 
+
 class Copilot:
     def __init__(self, key):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
-        embedding_model = HuggingFaceEmbedding(
-            model_name="BAAI/bge-small-en"
+        embedding_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en")
+        self.index = VectorStoreIndex.from_documents(
+            docs, embed_model=embedding_model, show_progress=True
         )
-        self.index = VectorStoreIndex.from_documents(docs, embed_model = embedding_model,
-                                                     show_progress=True)
-        self.retriever = self.index.as_retriever(
-                        similarity_top_k=3
-                        )
+        self.retriever = self.index.as_retriever(similarity_top_k=3)
 
-        self.llm_client = OpenAI(api_key = key)
-        
+        self.llm_client = OpenAI(api_key=key)
+
         self.system_prompt = """
-            You are an expert on Columbia University and your job is to answer questions 
-            about the university.
+            You are a knowledgeable researcher, explaining academic papers to PhD students.
         """
 
     def ask(self, question, messages):
@@ -44,42 +38,46 @@ class Copilot:
         nodes = self.retriever.retrieve(question)
         ### make answer a string with "1. <>, 2. <>, 3. <>"
         retrieved_info = "\n".join([f"{i+1}. {node.text}" for i, node in enumerate(nodes)])
-        
 
         processed_query_prompt = """
             The user is asking a question: {question}
 
             The retrived information is: {retrieved_info}
 
-            Please answer the question based on the retrieved information. If the question is not related to Columbia University, 
-            please tell the user and ask for a question related to Columbia University.
+            Please answer the question based on the retrieved information.
 
             Please highlight the information with bold text and bullet points.
         """
-        
-        processed_query = processed_query_prompt.format(question=question, 
-                                                        retrieved_info=retrieved_info)
-        
-        messages = [{"role": "system", "content": self.system_prompt}] + messages + [{"role": "user", "content": processed_query}]
-        response = chat_completion_request(self.llm_client, 
-                                           messages = messages, 
-                                           stream=True)
-        
+
+        processed_query = processed_query_prompt.format(
+            question=question, retrieved_info=retrieved_info
+        )
+
+        messages = (
+            [{"role": "system", "content": self.system_prompt}]
+            + messages
+            + [{"role": "user", "content": processed_query}]
+        )
+        response = chat_completion_request(self.llm_client, messages=messages, stream=True)
+
         return retrieved_info, response
+
 
 if __name__ == "__main__":
     ### get openai key from user input
     openai_api_key = os.getenv("OPENAI_API_KEY")
     if not openai_api_key:
-        openai_api_key = input("Please enter your OpenAI API Key (or set it as an environment variable OPENAI_API_KEY): ")
-    copilot = Copilot(key = openai_api_key)
+        openai_api_key = input(
+            "Please enter your OpenAI API Key (or set it as an environment variable OPENAI_API_KEY): "
+        )
+    copilot = Copilot(key=openai_api_key)
     messages = []
     while True:
         question = input("Please ask a question: ")
         retrived_info, answer = copilot.ask(question, messages=messages)
         ### answer can be a generator or a string
 
-        #print(retrived_info)
+        # print(retrived_info)
         if isinstance(answer, str):
             print(answer)
         else:
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,20 @@
-import streamlit as st
-from copilot import Copilot
 import os
+
+import streamlit as st
+
+from copilot import Copilot
+
 ### set openai key, first check if it is in environment variable, if not, check if it is in streamlit secrets, if not, raise error
 
 
-st.title("Chat with Columbia Copilot")
+# st.title("Chat with Columbia Copilot")
 st.write(
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
-if not openai_api_key: ### get openai key from user input
+if not openai_api_key:  ### get openai key from user input
     openai_api_key = st.text_input("Please enter your OpenAI API Key", type="password")
 
 if not openai_api_key:
@@ -19,21 +22,20 @@ if not openai_api_key:
 else:
     if "messages" not in st.session_state.keys():  # Initialize the chat messages history
         st.session_state.messages = [
-            {"role": "assistant", "content": "I am Columbia Copilot, your personal assistant. You can ask me about Columbia University."}
+            {
+                "role": "assistant",
+                "content": "Hi.",
+            }
         ]
 
     @st.cache_resource
     def load_copilot():
-        return Copilot(key = openai_api_key)
-
-
+        return Copilot(key=openai_api_key)
 
     if "chat_copilot" not in st.session_state.keys():  # Initialize the chat engine
         st.session_state.chat_copilot = load_copilot()
 
-    if prompt := st.chat_input(
-        "Ask a question"
-    ):  # Prompt for user input and save to chat history
+    if prompt := st.chat_input("Ask a question"):  # Prompt for user input and save to chat history
         st.session_state.messages.append({"role": "user", "content": prompt})
 
     for message in st.session_state.messages:  # Write message history to UI
@@ -44,10 +46,12 @@ else:
     if st.session_state.messages[-1]["role"] != "assistant":
         with st.chat_message("assistant"):
 
-            retrived_info, answer = st.session_state.chat_copilot.ask(prompt, messages=st.session_state.messages[:-1])
+            retrived_info, answer = st.session_state.chat_copilot.ask(
+                prompt, messages=st.session_state.messages[:-1]
+            )
             ### answer can be a generator or a string
 
-            #print(retrived_info)
+            # print(retrived_info)
             if isinstance(answer, str):
                 st.write(answer)
             else:
@@ -57,6 +61,7 @@ else:
                         content = chunk.choices[0].delta.content
                         if content:
                             yield content
+
                 answer = st.write_stream(generate())
 
             st.session_state.messages.append({"role": "assistant", "content": answer})
